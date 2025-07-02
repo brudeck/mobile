22ae6b5b385789612a318b25fba8483e9a7f4396
bella brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2025-07-02 08:08:06 -0300

    nh

---
+++ aula mobile/src/aula/aula1.java
@@ -0,0 +1,14 @@
+package aula;
+
+public class aula1 {
+   public static void main(String[] args) {
+      String nome;
+      String descricao;
+      String autor;
+      String isbn;
+      double preco;
+      String datapub;
+
+}
+}
+
---
+++ aula mobile/src/aula/aula2.java
@@ -0,0 +1,12 @@
+package aula;
+
+public class aula2 {
+    public static void main(String[] args) {
+       aula1 livro = new aula1();
+       livro.nome = "até o verão terminar";
+       livro.descricao = "uma obra literaria de romance";
+       livro.isbn =  "123456789";
+       livro.preco = 20.00;
+       livro.autor = "collen hoover";
+    }
+}
